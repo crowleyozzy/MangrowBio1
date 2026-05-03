fb423b2e2f9b9aed7b2718318266e712a878d5f8
crowleyozzy <crowleyozzy8@gmail.com>
2026-05-03 15:29:54 +0530

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -47,7 +47,7 @@ export default function Footer() {
             <h4>Global HQ</h4>
             <div className="fci">
               <svg viewBox="0 0 24 24"><path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/><path d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/></svg>
-              <span>100 PharmaTech Way<br/>Basel, CH-4002<br/>Switzerland</span>
+              <span>D417, Vishweshwara Nagar<br/>Mysore 570008</span>
             </div>
             <div className="fci">
               <svg viewBox="0 0 24 24"><path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
